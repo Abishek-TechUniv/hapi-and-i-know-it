ee5706223ee8aa748cf636affa0dfc3feac858ee
Abishek Aditya <abishekaditya@outlook.com>
2018-02-01 09:52:04 +0530

    Exercise 2 - add server test case

--- ex2/route.js
+++ ex2/route.js
@@ -1,5 +1,20 @@
 const prefix = name => `Hello ${name}`;
 
+const Hapi = require('hapi');
 
-module.exports = { prefix };
+const server = new Hapi.Server();
 
+server.connection({
+  host: 'localhost',
+  port: (Number(process.argv[2]) || 8081),
+});
+
+server.route({
+  path: '/{name}',
+  method: 'GET',
+  handler: (request, repsonse) => repsonse(prefix(request.params.name)),
+});
+
+// server.start();
+
+module.exports = { prefix, server };
--- ex2/route.test.js
+++ ex2/route.test.js
@@ -1,7 +1,13 @@
 const supertest = require('supertest');
-const { prefix } = require('./route');
+const { prefix, server } = require('./route');
 
 test('handler function should return Hello followed by name', () => {
   expect(prefix('Abishek')).toBe('Hello Abishek');
 });
 
+test('server to respond with result of sayHello function', (done) => {
+  supertest(server.listener).get('/abishek').then((response) => {
+    expect(response.text).toBe('Hello abishek');
+    done();
+  });
+});
